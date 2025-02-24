3542e2d3c96320acf620cbaf98d78e9f1d17568e
kamangir <arash@kamangir.net>
2025-02-24 14:05:00 -0800

    doc++ - kamangir/bolt#746

--- blue_rover/README.py
+++ blue_rover/README.py
@@ -8,9 +8,27 @@ from blue_rover import NAME, VERSION, ICON, REPO_NAME
 items = README.Items(
     [
         {
+            "name": "blue-rvr",
+            "marquee": "https://github.com/kamangir/blue-bracket/raw/main/images/blue1-1.jpg",
+            "description": "based on the [Sphero RVR SDK](https://github.com/sphero-inc/sphero-sdk-raspberrypi-python).",
+            "url": "https://github.com/kamangir/blue-rvr",
+        },
+        {
+            "name": "blue-donkey",
+            "marquee": "https://github.com/kamangir/blue-bracket/raw/main/images/blue-donkey-2.jpg",
+            "description": "based on [Donkey Car][https://docs.donkeycar.com/].",
+            "url": "https://github.com/kamangir/blue-bracket/blob/main/designs/blue-donkey.md",
+        },
+        {
+            "name": "skateboard",
+            "marquee": "https://github.com/kamangir/blue-bracket/raw/main/images/skateboard-1.jpg",
+            "description": "self-driving electric skateboard (concept)",
+            "url": "https://github.com/kamangir/blue-bracket/blob/main/designs/skateboard.md",
+        },
+        {
             "name": "blue-rover",
             "marquee": "https://github.com/waveshareteam/ugv_rpi/raw/main/media/UGV-Rover-details-23.jpg",
-            "description": "starting off on a [UGV Beast PI ROS2](https://www.waveshare.com/wiki/UGV_Beast_PI_ROS2)...",
+            "description": "[UGV Beast PI ROS2](https://www.waveshare.com/wiki/UGV_Beast_PI_ROS2).",
         },
     ]
 )
